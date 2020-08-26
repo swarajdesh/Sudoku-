804978d2540275ee484a450d9e28363906c8600d
swaraj <swarajdeshmukh45@gmail.com>
2020-08-26 16:08:56 +0530

    Problem Domain

---
+++ src/Sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,26 @@
+package Sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+
+    private final GameState gameState;
+    private  final int[] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+
+    public SudokuGame(GameState gameState,int[] gridState) {
+        this.gridState = gridState;
+        this.gameState = gameState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[] getGridState() {
+        return GameUtilities.copyToNewArray(gridState);
+        //returning copy of gridState this protects sudoku game object to mess with
+    }
+}
